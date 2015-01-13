4f990805eb2e2e0bdb4e4ceac242a3f40ad1823a
Alexey Akimov <alexvakimov@gmail.com>
2015-01-13 10:22:34 -0800

    Fixed a small bug in py-scr6.py related to convention of the Hprime matrix representation. Hprime is purely imaginary, so for plotting purposes (Hprime map, or absorption spectra) one has to use the imaginary part, which is proportional to transition dipole moments in p-representation.

--- code_level3/py-scr6.py
+++ code_level3/py-scr6.py
@@ -10,23 +10,26 @@ dE = 0.05
 tmin = 0
 tmax = 250
 
-excitation_spectrum.calculate("res/0_Ham_","_re","res/0_Hprime_","x_re",tmin,tmax,opt,scl1,scl2,"spectr1/ab_spectrx.dat",HOMO,minE,maxE,dE)
-excitation_spectrum.calculate("res/0_Ham_","_re","res/0_Hprime_","y_re",tmin,tmax,opt,scl1,scl2,"spectr1/ab_spectry.dat",HOMO,minE,maxE,dE)
-excitation_spectrum.calculate("res/0_Ham_","_re","res/0_Hprime_","z_re",tmin,tmax,opt,scl1,scl2,"spectr1/ab_spectrz.dat",HOMO,minE,maxE,dE)
+# Note a change with regard to earlier version!!! We use only imaginary part of Hprime to compute spectra. This is because Hprime 
+# now contains a Hamiltonian for field-matter interaction (transition dipole moment scaled by purely imaginary constant). So "x_re" is now
+# changed to "x_im", and so on
+excitation_spectrum.calculate("res/0_Ham_","_re","res/0_Hprime_","x_im",tmin,tmax,opt,scl1,scl2,"spectr1/ab_spectrx.dat",HOMO,minE,maxE,dE)
+excitation_spectrum.calculate("res/0_Ham_","_re","res/0_Hprime_","y_im",tmin,tmax,opt,scl1,scl2,"spectr1/ab_spectry.dat",HOMO,minE,maxE,dE)
+excitation_spectrum.calculate("res/0_Ham_","_re","res/0_Hprime_","z_im",tmin,tmax,opt,scl1,scl2,"spectr1/ab_spectrz.dat",HOMO,minE,maxE,dE)
 
 
-
+# Accordingly, we print only imaginary part of the Hprime for plotting, because the real part is zero!
 # Energy, couplings and H' in space of orbital indexes
 excitation_spectrum.ham_map("res/0_Ham_",   tmin,tmax,"_re" ,opt,scl1,"spectr1/ave_Ham_re.dat")
 excitation_spectrum.ham_map("res/0_Ham_",   tmin,tmax,"_im" ,opt,scl1,"spectr1/ave_Ham_im.dat")
-excitation_spectrum.ham_map("res/0_Hprime_",tmin,tmax,"x_re",opt,scl2,"spectr1/ave_Hprime_x_re.dat")
-excitation_spectrum.ham_map("res/0_Hprime_",tmin,tmax,"y_re",opt,scl2,"spectr1/ave_Hprime_y_re.dat")
-excitation_spectrum.ham_map("res/0_Hprime_",tmin,tmax,"z_re",opt,scl2,"spectr1/ave_Hprime_z_re.dat")
+excitation_spectrum.ham_map("res/0_Hprime_",tmin,tmax,"x_im",opt,scl2,"spectr1/ave_Hprime_x_im.dat")
+excitation_spectrum.ham_map("res/0_Hprime_",tmin,tmax,"y_im",opt,scl2,"spectr1/ave_Hprime_y_im.dat")
+excitation_spectrum.ham_map("res/0_Hprime_",tmin,tmax,"z_im",opt,scl2,"spectr1/ave_Hprime_z_im.dat")
 
 # Same, but in space of orbital energies
 excitation_spectrum.ham_map1("res/0_Ham_","_re","res/0_Ham_",   tmin,tmax,"_re" ,opt,scl1,scl1,"spectr1/1ave_Ham_re.dat")
 excitation_spectrum.ham_map1("res/0_Ham_","_re","res/0_Ham_",   tmin,tmax,"_im" ,opt,scl1,scl1,"spectr1/1ave_Ham_im.dat")
-excitation_spectrum.ham_map1("res/0_Ham_","_re","res/0_Hprime_",tmin,tmax,"x_re",opt,scl1,scl2,"spectr1/1ave_Hprime_x_re.dat")
-excitation_spectrum.ham_map1("res/0_Ham_","_re","res/0_Hprime_",tmin,tmax,"y_re",opt,scl1,scl2,"spectr1/1ave_Hprime_y_re.dat")
-excitation_spectrum.ham_map1("res/0_Ham_","_re","res/0_Hprime_",tmin,tmax,"z_re",opt,scl1,scl2,"spectr1/1ave_Hprime_z_re.dat")
+excitation_spectrum.ham_map1("res/0_Ham_","_re","res/0_Hprime_",tmin,tmax,"x_im",opt,scl1,scl2,"spectr1/1ave_Hprime_x_im.dat")
+excitation_spectrum.ham_map1("res/0_Ham_","_re","res/0_Hprime_",tmin,tmax,"y_im",opt,scl1,scl2,"spectr1/1ave_Hprime_y_im.dat")
+excitation_spectrum.ham_map1("res/0_Ham_","_re","res/0_Hprime_",tmin,tmax,"z_im",opt,scl1,scl2,"spectr1/1ave_Hprime_z_im.dat")
 
